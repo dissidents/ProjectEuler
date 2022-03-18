f78457a02b1df2e9ecb9d87f172619cade1f1332
LegenDaVinci <su11198@studenti.polito.it>
2022-03-18 23:31:57 +0500

    Problem_6

--- Problems/LegenDaVinci/Problem_6.cpp
+++ Problems/LegenDaVinci/Problem_6.cpp
@@ -18,14 +18,14 @@ public:
 
     void Compute() override
     {
-        for (int i = 1; i < 100; ++i){
-            sumOfSquare += i ^ 2;
+        for (int i = 1; i < 101; ++i){
+            sumOfSquare += pow(i,2);
         }
-        for (int j = 1; j < 100; ++j){
+        for (int j = 1; j < 101; ++j){
             squareOfSum += j;
         }
         squareOfSum = pow(squareOfSum,2);
-        answer = to_string(squareOfSum-sumOfSquare);
+        answer = to_string(int(squareOfSum-sumOfSquare));
     }
 
 };
